88491864af2bfcbd4a849ae0ee3cb24433bd913c
SarahMay Harel <sarahmay@bowtie.co>
2022-10-17 15:16:19 -0400

    WIP dark mode

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,8 @@ checkbox.addEventListener('change', function () {
 
 if (localStorage.getItem('dark-mode') == 'true') {
     body.classList.add('dark-mode');
+} else {
+    body.classList.remove('dark-mode');
 }
 
 
@@ -33,40 +35,55 @@ if (testingLocalStorage != 'false') {
 
 //Delete and insert leaf or stars depending on dark mode
 
-function foo() {
-    let leafElement = document.getElementById('leafJs');
-    let starsElement = document.getElementById("starsJs")
-    let bodyElement = document.getElementById("bodyID")
+// function foo() {
+    //let leafElement = document.getElementById('leafJs');
+   // let starsElement = document.getElementById("starsJs")
+    //let bodyElement = document.getElementById("bodyID")
 
-    if (bodyElement.classList.contains("dark-mode")) {
+   // if (localStorage.getItem('dark-mode') == 'true') {
 
-        //test
-        console.log("Helppppp")
+        //Test
+        //console.log("Dark mode is on")
 
-        // removes the leaf png if Dark Mode is on (not working)
-        leafElement.remove();
+        //Removes the leaf png if Dark Mode is on (not working)
+       // leafElement.remove();
+
+        //Insert DOM element
+        // const newNode = document.createElement("div")
+        // const textNode = document.createTextNode("Buffy is the cutest")
+
+        // newNode.appendChild(textNode);
+
+        // const list = document.getElementById("imageCol")
+        // list.insertBefore(newNode, null)
+
 
         //Create missing image element to toggle images back on and off as Dark Mode is turned on and off (not working)
-
         // var img = document.createElement('img');
         // img.src="assets/img/—Pngtree—sparkling stars shiny sparks yellow_6320234.png"
         // document.getElementById('imageCol').appendChild('img');
 
-    } else {
-        // removes the stars element if Dark Mode is NOT on (working)
-        starsElement.remove();
+   // } else {
+        //Test
+        //console.log("Dark mode is off")
+
+        //Removes the stars element if Dark Mode is NOT on (working)
+      //  starsElement.remove();
+
+
+        //Check after a certain amount of time whether to remove the stars
         // function bar() {
         //     document.getElementById("starsJs").remove();
         // }
         // setTimeout(bar, 2000);
 
-        //Create missing image element to toggle images back on and off as Dark Mode is turned on and off (not working)
 
+        //Create missing image element to toggle images back on and off as Dark Mode is turned on and off (not working)
         // var img = document.createElement('img');
         // img.src="https://i.imgur.com/mhGjsHt.png"
         // document.getElementById('imageCol').appendChild('img');
-    }
-   setTimeout(foo, 1000);
-}
+  //  }
+   //setTimeout(foo, 2000);
+//}
 
-foo();
+// foo();
